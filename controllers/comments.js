18f34f8cec9d21df4e22e76f2380f66d76781b2a
Sarah J Dawns <123122262+sarahJDawns@users.noreply.github.com>
2023-04-09 16:59:43 -0600

    we are good here

--- controllers/comments.js
+++ controllers/comments.js
@@ -15,29 +15,3 @@ module.exports = {
     }
   },
 };
-
-// module.exports = {
-//   createComment: async (req, res) => {
-//     try {
-//       const comment = new Comment({
-//         comment: req.body.comment,
-//         likes: 0,
-//         post: req.params.id,
-//         user: req.user,
-//       });
-//       const comments = await Comment.find({ post: req.params.id })
-//         .sort({ createdAt: "desc" })
-//         .populate("user")
-//         .lean();
-//       await comment.populate("user");
-//       comment.user = await User.findById(comment.user).exec();
-//       await comment.save();
-//       console.log("Comment has been added!");
-//       console.log(comment.user);
-//       console.log(comment.user.userName);
-//       res.redirect("/post/", +req.params.id);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   },
-// };
--- models/Comment.js
+++ models/Comment.js
@@ -13,18 +13,10 @@ const CommentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
   },
-  // user: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  // },
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
-
-// CommentSchema.pre("find", function () {
-//   this.populate("user");
-// });
 
 module.exports = mongoose.model("Comment", CommentSchema);
