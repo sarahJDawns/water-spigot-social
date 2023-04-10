08b8e1a66e467276a371b822fc466247dc9d22ce
Sarah J Dawns <123122262+sarahJDawns@users.noreply.github.com>
2023-04-10 15:24:20 -0600

    error msg submit & delete acct

--- models/Comment.js
+++ models/Comment.js
@@ -15,6 +15,7 @@ const CommentSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
+    default: Date.now,
   },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 });
